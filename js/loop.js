2d2f62d0d266441e82608f36f6460350378b97c1
callenhinds <callenhinds2685@mysvc.skagit.edu>
2026-04-28 17:01:00 -0700

    Update loop.js

--- js/loop.js
+++ js/loop.js
@@ -1,35 +1,37 @@
-var service = 'va'; // 'va' or 'web'
-var i = 1;
-var msg = '';
+function chooseService(service) {
 
-if (service === 'va') {
-  while (i < 6) {
-    msg += 'Task ' + i + ': ';
+  var i = 1;
+  var msg = '';
 
-    if (i === 1) msg += 'Inbox management';
-    else if (i === 2) msg += 'Calendar scheduling';
-    else if (i === 3) msg += 'Customer support replies';
-    else if (i === 4) msg += 'Data entry & organization';
-    else if (i === 5) msg += 'Research assistance';
+  if (service === 'va') {
+    while (i < 6) {
+      msg += 'Task ' + i + ': ';
 
-    msg += '<br />';
-    i++;
+      if (i === 1) msg += 'Inbox management';
+      else if (i === 2) msg += 'Calendar scheduling';
+      else if (i === 3) msg += 'Customer support replies';
+      else if (i === 4) msg += 'Data entry & organization';
+      else if (i === 5) msg += 'Research assistance';
+
+      msg += '<br />';
+      i++;
+    }
+
+  } else {
+    while (i < 6) {
+      msg += 'Feature ' + i + ': ';
+
+      if (i === 1) msg += 'Responsive design';
+      else if (i === 2) msg += 'Fast loading speeds';
+      else if (i === 3) msg += 'SEO optimization';
+      else if (i === 4) msg += 'Modern UI/UX';
+      else if (i === 5) msg += 'Contact forms & integrations';
+
+      msg += '<br />';
+      i++;
+    }
   }
 
-} else {
-  while (i < 6) {
-    msg += 'Feature ' + i + ': ';
-
-    if (i === 1) msg += 'Responsive design';
-    else if (i === 2) msg += 'Fast loading speeds';
-    else if (i === 3) msg += 'SEO optimization';
-    else if (i === 4) msg += 'Modern UI/UX';
-    else if (i === 5) msg += 'Contact forms & integrations';
-
-    msg += '<br />';
-    i++;
-  }
+  var el = document.getElementById('blackboard');
+  el.innerHTML = msg;
 }
-
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
