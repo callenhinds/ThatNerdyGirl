c77260efdded00ac457b608e02234f90c36cca41
callenhinds <callenhinds2685@mysvc.skagit.edu>
2026-05-04 12:49:03 -0700

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,11 @@
-const themes = ["light", "dark", "fun"];
+const themes = ["dark", "light", "fun"];
 let currentTheme = 0;
 
-document.getElementById("themeBtn").addEventListener("click", () => {
+document.body.classList.add(themes[currentTheme]);
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
   document.body.classList.remove(themes[currentTheme]);
 
   currentTheme = (currentTheme + 1) % themes.length;
